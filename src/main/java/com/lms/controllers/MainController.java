153239be5f69bcb4902989c4f5de0ba481110577
DESKTOP-431IURL\JUSTIN <chuksjustin2000@gmail.com>
2018-05-11 17:20:44 +0100

    My Spring Boot Application with CRUD functionalities with vies and controllers

---
+++ src/main/java/com/lms/controllers/MainController.java
@@ -0,0 +1,83 @@
+package com.lms.controllers;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lms.models.Book;
+import com.lms.services.LmsService;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	private LmsService lmsService;
+	
+	@GetMapping(value="/")
+	public ModelAndView init(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView("index");
+		
+		//Collection<Book> books = lmsService.findAllBooks();
+		
+		mv.addObject("books", lmsService.findAllBooks());
+		req.setAttribute("mode", "BOOK_VIEW");
+		
+		return mv;
+	}
+	
+	@GetMapping(value="/update")
+	public ModelAndView init(@RequestParam long id, HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView("index");
+		
+		
+		Book book = lmsService.findOne(id);
+		
+		mv.addObject("book", book);
+		req.setAttribute("mode", "BOOK_EDIT");
+		
+		return mv;
+	}
+	
+	@GetMapping(value="/newBook")
+	public ModelAndView create( HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView("index");
+		
+		
+		Book book = new Book();
+		
+		mv.addObject("book", book);
+		req.setAttribute("mode", "BOOK_NEW");
+		
+		return mv;
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-mm-dd"), false));
+	}
+	
+	@PostMapping(value="/save")
+	public void update(@ModelAttribute Book book, BindingResult result, HttpServletResponse res) throws IOException{
+		
+		lmsService.save(book);
+
+		res.sendRedirect("/");
+	}
+
+}
---
+++ src/main/java/com/lms/services/LmsService.java
@@ -0,0 +1,42 @@
+package com.lms.services;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lms.dao.LmsRepository;
+import com.lms.models.Book;
+
+@Service
+public class LmsService {
+	
+	@Autowired
+	private LmsRepository lmsRepository;
+	
+	public Collection<Book> findAllBooks(){
+		List<Book> books = new ArrayList<>();
+		for (Book book : lmsRepository.findAll()) {
+			books.add(book);
+		}
+		
+		return books;
+	}
+	
+	public void delete(long id) {
+		lmsRepository.delete(id);
+		
+	}
+	
+	public Book findOne(long id)  {
+		return lmsRepository.findOne(id);
+		//return lmsRepository.findById(id).orElseThrow(()-> new NotFound());
+	}
+	
+	public void save(Book book) {
+		lmsRepository.save(book);
+	}
+
+}
